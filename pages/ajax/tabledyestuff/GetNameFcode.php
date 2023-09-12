44e51ed68582f74d8f5b3a4a275d656578c93a87
Nilokusumo Tri Pamungkas <44149646+Nktpamungkas@users.noreply.github.com>
2023-09-12 16:36:20 +0700

    perubahan metode pengambilan json

--- pages/ajax/tabledyestuff/GetNameFcode.php
+++ pages/ajax/tabledyestuff/GetNameFcode.php
@@ -3,7 +3,7 @@ ini_set("error_reporting", 1);
 include "../../../koneksi.php";
 
 $code = $_POST['code'];
-$sql = mysqli_query($con,"SELECT Product_Name, Product_Unit from tbl_dyestuff where code = '$code' and `is_active` = 'TRUE'");
+$sql = mysqli_query($con,"SELECT Product_Name, Product_Unit, ket from tbl_dyestuff where code = '$code' and `is_active` = 'TRUE'");
 $result = mysqli_fetch_array($sql);
 if ($result["Product_Unit"] == 0) {
     $uom = "Gr/L";
@@ -12,9 +12,20 @@ if ($result["Product_Unit"] == 0) {
 }
 
 if ($result["Product_Name"] == "-----------------------") {
-    $resultn = $result["Product_Name"];
+    $resultn = array(
+        'Product_Name'  => $result["Product_Name"]
+    );
 } else {
-    $resultn = $result["Product_Name"] . ' (' . $uom . ')';
+    if($result["ket"] == "Suhu"){
+        $resultn = array(
+            'ket'            => $result['ket'],
+            'Product_Name'   => $result["Product_Name"]
+        );
+    }else{
+        $resultn = array(
+            'Product_Name'  => $result["Product_Name"] . ' (' . $uom . ')'
+        );
+    }
 }
 
 $response = json_encode($resultn);
