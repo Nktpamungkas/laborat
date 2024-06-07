a0c1f6023b56e663a26e4bee1478a070df91122b
Nktpamungkas <nktpamungkas28@gmail.com>
2024-06-07 10:55:39 +0700

    Update utk perhitungan matching development dan tambahkan SUB TOTAL

--- pages/ajax/before_switch_day.php
+++ pages/ajax/before_switch_day.php
@@ -62,7 +62,7 @@ include '../../koneksi.php';
 </div>
 <div class="col-md-6">
     <div class="box">
-        <h4 class="text-center" style="font-weight: bold;">OUTPUT PER GROUP H-1 <span class="text-center" style="font-weight: bold;">H-1</span></h4>
+        <h4 class="text-center" style="font-weight: bold;">OUTPUT PER GROUP <span class="text-center" style="font-weight: bold;">H-1</span></h4>
         <table class="table table-chart">
             <thead>
                 <tr align="center">
@@ -115,50 +115,60 @@ include '../../koneksi.php';
                     <td align="center"><?php $A_LD = get_val('L/D', 'A') + get_val('LD NOW', 'A'); echo $A_LD; ?></td>
                     <td align="center"><?php $A_MU = get_val('Matching Ulang', 'A') + get_val('Matching Ulang NOW', 'A'); echo $A_MU; ?></td>
                     <td align="center"><?php $A_P = get_val('Perbaikan', 'A') + get_val('Perbaikan NOW', 'A'); echo $A_P; ?></td>
-                    <td align="center"><?php $A_D = get_val('Matching Development', 'A'); echo $A_D; ?></td>
+                    <td align="center"><?php $A_D = get_val('Matching Development', 'A') + get_val('Matching Development NOW', 'A'); echo $A_D; ?></td>
                     <td align="center"><?= $A_LD + $A_MU + $A_P + $A_D; ?></td>
                 </tr>
                 <tr>
                     <td align="center">Group B</td>
-                    <td align="center"><?php $A_LD = get_val('L/D', 'B') + get_val('LD NOW', 'B'); echo $A_LD; ?></td>
-                    <td align="center"><?php $A_MU = get_val('Matching Ulang', 'B') + get_val('Matching Ulang NOW', 'B'); echo $A_MU; ?></td>
-                    <td align="center"><?php $A_P = get_val('Perbaikan', 'B') + get_val('Perbaikan NOW', 'B'); echo $A_P; ?></td>
-                    <td align="center"><?php $A_D = get_val('Matching Development', 'B'); echo $A_D; ?></td>
-                    <td align="center"><?= $A_LD + $A_MU + $A_P + $A_D; ?></td>
+                    <td align="center"><?php $B_LD = get_val('L/D', 'B') + get_val('LD NOW', 'B'); echo $B_LD; ?></td>
+                    <td align="center"><?php $B_MU = get_val('Matching Ulang', 'B') + get_val('Matching Ulang NOW', 'B'); echo $B_MU; ?></td>
+                    <td align="center"><?php $B_P = get_val('Perbaikan', 'B') + get_val('Perbaikan NOW', 'B'); echo $B_P; ?></td>
+                    <td align="center"><?php $B_D = get_val('Matching Development', 'B') + get_val('Matching Development NOW', 'B'); echo $B_D; ?></td>
+                    <td align="center"><?= $B_LD + $B_MU + $B_P + $B_D; ?></td>
                 </tr>
                 <tr>
                     <td align="center">Group C</td>
-                    <td align="center"><?php $A_LD = get_val('L/D', 'C') + get_val('LD NOW', 'C'); echo $A_LD; ?></td>
-                    <td align="center"><?php $A_MU = get_val('Matching Ulang', 'C') + get_val('Matching Ulang NOW', 'C'); echo $A_MU; ?></td>
-                    <td align="center"><?php $A_P = get_val('Perbaikan', 'C') + get_val('Perbaikan NOW', 'C'); echo $A_P; ?></td>
-                    <td align="center"><?php $A_D = get_val('Matching Development', 'C'); echo $A_D; ?></td>
-                    <td align="center"><?= $A_LD + $A_MU + $A_P + $A_D; ?></td>
+                    <td align="center"><?php $C_LD = get_val('L/D', 'C') + get_val('LD NOW', 'C'); echo $C_LD; ?></td>
+                    <td align="center"><?php $C_MU = get_val('Matching Ulang', 'C') + get_val('Matching Ulang NOW', 'C'); echo $C_MU; ?></td>
+                    <td align="center"><?php $C_P = get_val('Perbaikan', 'C') + get_val('Perbaikan NOW', 'C'); echo $C_P; ?></td>
+                    <td align="center"><?php $C_D = get_val('Matching Development', 'C') + get_val('Matching Development NOW', 'C'); echo $C_D; ?></td>
+                    <td align="center"><?= $C_LD + $C_MU + $C_P + $C_D; ?></td>
                 </tr>
                 <tr>
                     <td align="center">Group D</td>
-                    <td align="center"><?php $A_LD = get_val('L/D', 'D') + get_val('LD NOW', 'D'); echo $A_LD; ?></td>
-                    <td align="center"><?php $A_MU = get_val('Matching Ulang', 'D') + get_val('Matching Ulang NOW', 'D'); echo $A_MU; ?></td>
-                    <td align="center"><?php $A_P = get_val('Perbaikan', 'D') + get_val('Perbaikan NOW', 'D'); echo $A_P; ?></td>
-                    <td align="center"><?php $A_D = get_val('Matching Development', 'D'); echo $A_D; ?></td>
-                    <td align="center"><?= $A_LD + $A_MU + $A_P + $A_D; ?></td>
+                    <td align="center"><?php $D_LD = get_val('L/D', 'D') + get_val('LD NOW', 'D'); echo $D_LD; ?></td>
+                    <td align="center"><?php $D_MU = get_val('Matching Ulang', 'D') + get_val('Matching Ulang NOW', 'D'); echo $D_MU; ?></td>
+                    <td align="center"><?php $D_P = get_val('Perbaikan', 'D') + get_val('Perbaikan NOW', 'D'); echo $D_P; ?></td>
+                    <td align="center"><?php $D_D = get_val('Matching Development', 'D') + get_val('Matching Development NOW', 'D'); echo $D_D; ?></td>
+                    <td align="center"><?= $D_LD + $D_MU + $D_P + $D_D; ?></td>
                 </tr>
                 <tr>
                     <td align="center">Group E</td>
-                    <td align="center"><?php $A_LD = get_val('L/D', 'E') + get_val('LD NOW', 'E'); echo $A_LD; ?></td>
-                    <td align="center"><?php $A_MU = get_val('Matching Ulang', 'E') + get_val('Matching Ulang NOW', 'E'); echo $A_MU; ?></td>
-                    <td align="center"><?php $A_P = get_val('Perbaikan', 'E') + get_val('Perbaikan NOW', 'E'); echo $A_P; ?></td>
-                    <td align="center"><?php $A_D = get_val('Matching Development', 'E'); echo $A_D; ?></td>
-                    <td align="center"><?= $A_LD + $A_MU + $A_P + $A_D; ?></td>
+                    <td align="center"><?php $E_LD = get_val('L/D', 'E') + get_val('LD NOW', 'E'); echo $E_LD; ?></td>
+                    <td align="center"><?php $E_MU = get_val('Matching Ulang', 'E') + get_val('Matching Ulang NOW', 'E'); echo $E_MU; ?></td>
+                    <td align="center"><?php $E_P = get_val('Perbaikan', 'E') + get_val('Perbaikan NOW', 'E'); echo $E_P; ?></td>
+                    <td align="center"><?php $E_D = get_val('Matching Development', 'E') + get_val('Matching Development NOW', 'E'); echo $E_D; ?></td>
+                    <td align="center"><?= $E_LD + $E_MU + $E_P + $E_D; ?></td>
                 </tr>
                 <tr>
                     <td align="center">Group F</td>
-                    <td align="center"><?php $A_LD = get_val('L/D', 'F') + get_val('LD NOW', 'F'); echo $A_LD; ?></td>
-                    <td align="center"><?php $A_MU = get_val('Matching Ulang', 'F') + get_val('Matching Ulang NOW', 'F'); echo $A_MU; ?></td>
-                    <td align="center"><?php $A_P = get_val('Perbaikan', 'F') + get_val('Perbaikan NOW', 'F'); echo $A_P; ?></td>
-                    <td align="center"><?php $A_D = get_val('Matching Development', 'F'); echo $A_D; ?></td>
-                    <td align="center"><?= $A_LD + $A_MU + $A_P + $A_D; ?></td>
+                    <td align="center"><?php $F_LD = get_val('L/D', 'F') + get_val('LD NOW', 'F'); echo $F_LD; ?></td>
+                    <td align="center"><?php $F_MU = get_val('Matching Ulang', 'F') + get_val('Matching Ulang NOW', 'F'); echo $F_MU; ?></td>
+                    <td align="center"><?php $F_P = get_val('Perbaikan', 'F') + get_val('Perbaikan NOW', 'F'); echo $F_P; ?></td>
+                    <td align="center"><?php $F_D = get_val('Matching Development', 'F') + get_val('Matching Development NOW', 'F'); echo $F_D; ?></td>
+                    <td align="center"><?= $F_LD + $F_MU + $F_P + $F_D; ?></td>
                 </tr>
             </tbody>
+            <tfoot>
+                <tr>
+                    <th>SUB TOTAL</th>
+                    <th align="center"><?= $T_LD = $A_LD + $B_LD + $C_LD + $D_LD + $E_LD + $F_LD; ?></th>
+                    <th align="center"><?= $T_MU = $A_MU + $B_MU + $C_MU + $D_MU + $E_MU + $F_MU; ?></th>
+                    <th align="center"><?= $T_P  = $A_P + $B_P + $C_P + $D_P + $E_P + $F_P; ?></th>
+                    <th align="center"><?= $T_D  = $A_D + $B_D + $C_D + $D_D + $E_D + $F_D; ?></th>
+                    <th align="center"><?= $T_LD + $T_MU + $T_P + $T_D; ?></th>
+                </tr>
+            </tfoot>
         </table>
         <!-- <h4 class="text-center" style="font-weight: bold;">RECAP PENANGGUNG JAWAB <span class="text-center" style="font-weight: bold;">H-1</span></h4> -->
         <!-- <table class="table table-chart">
