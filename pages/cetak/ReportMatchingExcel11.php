b4768288cbefad1c52c0c1c7c89e384987236ede
Nktpamungkas <nktpamungkas28@gmail.com>
2024-06-18 08:32:27 +0700

    benerin laporan schedule otomatis nya laborat

--- pages/cetak/ReportMatchingExcel11.php
+++ pages/cetak/ReportMatchingExcel11.php
@@ -12,85 +12,98 @@ include "../../koneksi.php";
 ?>
 <table>
               <tr>
-                <th>no_resep</th>
-                <th>no_order</th>
-                <th>no_po</th>
-                <th>langganan</th>
-                <th>no_item</th>
-                <th>jenis_kain</th>
-                <th>benang</th>
-                <th>cocok_warna</th>
-                <th>warna</th>
-                <th>no_warna</th>
-                <th>lebar</th>
-                <th>gramasi</th>
-                <th>qty_order</th>
-                <th>status_bagi</th>
-                <th>tgl_in</th>
-                <th>tgl_out</th>
-                <th>proses</th>
-                <th>buyer</th>
-                <th>tgl_delivery</th>
-                <th>note</th>
-                <th>jenis_matching</th>
-                <th>tgl_buat</th>
-                <th>created_by</th>
-                <th>tgl_update</th>
-                <th>last_update_by</th>
-                <th>grup</th>
-                <th>matcher</th>
-                <th>cek_warna</th>
-                <th>cek_dye</th>
-                <th>status</th>
-                <th>kt_status</th>
-                <th>koreksi_resep</th>
-                <th>percobaan_ke</th>
-                <th>percobaan_berapa_kali</th>
-                <th>benang_aktual</th>
-                <th>lebar_aktual</th>
-                <th>gramasi_aktual</th>
-                <th>ph</th>
-                <th>soaping_sh</th>
-                <th>soaping_tm</th>
-                <th>rc_sh</th>
-                <th>rc_tm</th>
-                <th>lr</th>
-                <th>cie_wi</th>
-                <th>cie_tint</th>
-                <th>spektro_r</th>
-                <th>ket</th>
-                <th>cside_c</th>
-                <th>cside_min</th>
-                <th>tside_c</th>
-                <th>tside_min</th>
-                <th>done_matching</th>
-                <th>created_at</th>
-                <th>created_by</th>
-                <th>edited_at</th>
-                <th>edited_by</th>
-                <th>target_selesai</th>
-                <th>mulai_by</th>
-                <th>mulai_at</th>
-                <th>selesai_by</th>
-                <th>selesai_at</th>
-                <th>approve_by</th>
-                <th>approve_at</th>
-                <th>approve</th>
-                <th>hold_at</th>
-                <th>hold_by</th>
-                <th>timer</th>
-                <th>why_batal</th>
-                <th>revisi_at</th>
-                <th>revisi_by</th>
-                <th>kadar_air</th>
-                <th>final_matcher</th>
-                <th>colorist1</th>
-                <th>colorist2</th>
-                <th>penanggung_jawab</th>
-                <th>bleaching_tm</th>
-                <th>bleaching_sh</th>
-                <th>second_lr</th>
-                <th>remark_dye</th>
+              <th class="text-center">no_resep</th>
+                <th class="text-center">no_order</th>
+                <th class="text-center">no_po</th>
+                <th class="text-center">langganan</th>
+                <th class="text-center">no_item</th>
+                <th class="text-center">jenis_kain</th>
+                <th class="text-center">benang</th>
+                <th class="text-center">cocok_warna</th>
+                <th class="text-center">warna</th>
+                <th class="text-center">no_warna</th>
+                <th class="text-center">lebar</th>
+                <th class="text-center">gramasi</th>
+                <th class="text-center">qty_order</th>
+                <th class="text-center">status_bagi</th>
+                <th class="text-center">tgl_in</th>
+                <th class="text-center">tgl_out</th>
+                <th class="text-center">proses</th>
+                <th class="text-center">buyer</th>
+                <th class="text-center">tgl_delivery</th>
+                <th class="text-center">note</th>
+                <th class="text-center">jenis_matching</th>
+                <th class="text-center">tgl_buat</th>
+                <th class="text-center">created_by</th>
+                <th class="text-center">tgl_update</th>
+                <th class="text-center">last_update_by</th>
+                <th class="text-center">grup</th>
+                <th class="text-center">matcher</th>
+                <th class="text-center">cek_warna</th>
+                <th class="text-center">cek_dye</th>
+                <th class="text-center">status</th>
+                <th class="text-center">kt_status</th>
+                <th class="text-center ">koreksi_resep 1</th>
+                <th class="text-center ">koreksi_resep 1</th>
+                <th class="text-center ">koreksi_resep 2</th>
+                <th class="text-center ">koreksi_resep 2</th>
+                <th class="text-center ">koreksi_resep 3</th>
+                <th class="text-center ">koreksi_resep 3</th>
+                <th class="text-center ">koreksi_resep 4</th>
+                <th class="text-center ">koreksi_resep 4</th>
+                <th class="text-center">percobaan_ke</th>
+                <th class="text-center">percobaan_berapa_kali</th>
+                <th class="text-center">benang_aktual</th>
+                <th class="text-center">lebar_aktual</th>
+                <th class="text-center">gramasi_aktual</th>
+                <th class="text-center">ph</th>
+                <th class="text-center">soaping_sh</th>
+                <th class="text-center">soaping_tm</th>
+                <th class="text-center">rc_sh</th>
+                <th class="text-center">rc_tm</th>
+                <th class="text-center">lr</th>
+                <th class="text-center">cie_wi</th>
+                <th class="text-center">cie_tint</th>
+                <th class="text-center">spektro_r</th>
+                <th class="text-center">ket</th>
+                <th class="text-center">cside_c</th>
+                <th class="text-center">cside_min</th>
+                <th class="text-center">tside_c</th>
+                <th class="text-center">tside_min</th>
+                <th class="text-center">done_matching</th>
+                <th class="text-center">created_at</th>
+                <th class="text-center">created_by</th>
+                <th class="text-center">edited_at</th>
+                <th class="text-center">edited_by</th>
+                <th class="text-center">target_selesai</th>
+                <th class="text-center">mulai_by</th>
+                <th class="text-center">mulai_at</th>
+                <th class="text-center">selesai_by</th>
+                <th class="text-center">selesai_at</th>
+                <th class="text-center">approve_by</th>
+                <th class="text-center">approve_at</th>
+                <th class="text-center">approve</th>
+                <th class="text-center">hold_at</th>
+                <th class="text-center">hold_by</th>
+                <th class="text-center">timer</th>
+                <th class="text-center">why_batal</th>
+                <th class="text-center">revisi_at</th>
+                <th class="text-center">revisi_by</th>
+                <th class="text-center">kadar_air</th>
+                <th class="text-center">final_matcher</th>
+                <th class="text-center ">colorist 1</th>
+                <th class="text-center ">colorist 1</th>
+                <th class="text-center ">colorist 2</th>
+                <th class="text-center ">colorist 2</th>
+                <th class="text-center ">colorist 3</th>
+                <th class="text-center ">colorist 3</th>
+                <th class="text-center ">colorist 4</th>
+                <th class="text-center ">colorist 4</th>
+                <th class="text-center">penanggung_jawab</th>
+                <th class="text-center">bleaching_tm</th>
+                <th class="text-center">bleaching_sh</th>
+                <th class="text-center">second_lr</th>
+                <th class="text-center">remark_dye</th>
               </tr>
               <?php
               $date_s = date('Y-m-d', strtotime("-1 days"));
@@ -117,31 +130,31 @@ include "../../koneksi.php";
                 $no++;
               ?>
                 <tr>
-                  <td><?php echo $r['no_resep'] ?></td>
-                  <td><?php echo $r['no_order'] ?></td>
-                  <td><?php echo $r['no_po'] ?></td>
-                  <td><?php echo $r['langganan'] ?></td>
-                  <td><?php echo $r['no_item'] ?></td>
-                  <td><?php echo $r['jenis_kain'] ?></td>
-                  <td><?php echo $r['benang'] ?></td>
-                  <td><?php echo $r['cocok_warna'] ?></td>
-                  <td><?php echo $r['warna'] ?></td>
-                  <td><?php echo $r['no_warna'] ?></td>
-                  <td><?php echo $r['lebar'] ?></td>
-                  <td><?php echo $r['gramasi'] ?></td>
-                  <td><?php echo $r['qty_order'] ?></td>
-                  <td><?php echo $r['status_bagi'] ?></td>
-                  <td><?php echo $r['tgl_in'] ?></td>
-                  <td><?php echo $r['tgl_out'] ?></td>
-                  <td><?php echo $r['proses'] ?></td>
-                  <td><?php echo $r['buyer'] ?></td>
-                  <td><?php echo $r['tgl_delivery'] ?></td>
-                  <td><?php echo $r['note'] ?></td>
-                  <td><?php echo $r['jenis_matching'] ?></td>
-                  <td><?php echo $r['tgl_buat'] ?></td>
-                  <td><?php echo $r['created_by'] ?></td>
-                  <td><?php echo $r['tgl_update'] ?></td>
-                  <td><?php echo $r['last_update_by'] ?></td>
+                  <td> <?php echo $r['no_resep'] ?></td>
+                  <td> <?php echo $r['no_order'] ?></td>
+                  <td> <?php echo $r['no_po'] ?></td>
+                  <td> <?php echo $r['langganan'] ?></td>
+                  <td> <?php echo $r['no_item'] ?></td>
+                  <td> <?php echo $r['jenis_kain'] ?></td>
+                  <td> <?php echo $r['benang'] ?></td>
+                  <td> <?php echo $r['cocok_warna'] ?></td>
+                  <td> <?php echo $r['warna'] ?></td>
+                  <td> <?php echo $r['no_warna'] ?></td>
+                  <td> <?php echo $r['lebar'] ?></td>
+                  <td> <?php echo $r['gramasi'] ?></td>
+                  <td> <?php echo $r['qty_order'] ?></td>
+                  <td> <?php echo $r['status_bagi'] ?></td>
+                  <td> <?php echo $r['tgl_in'] ?></td>
+                  <td> <?php echo $r['tgl_out'] ?></td>
+                  <td> <?php echo $r['proses'] ?></td>
+                  <td> <?php echo $r['buyer'] ?></td>
+                  <td> <?php echo $r['tgl_delivery'] ?></td>
+                  <td> <?php echo $r['note'] ?></td>
+                  <td> <?php echo $r['jenis_matching'] ?></td>
+                  <td> <?php echo $r['tgl_buat'] ?></td>
+                  <td> <?php echo $r['created_by'] ?></td>
+                  <td> <?php echo $r['tgl_update'] ?></td>
+                  <td> <?php echo $r['last_update_by'] ?></td>
                   <td><?php echo $r['grp'] ?></td>
                   <td><?php echo $r['matcher'] ?></td>
                   <td><?php echo $r['cek_warna'] ?></td>
@@ -149,6 +162,13 @@ include "../../koneksi.php";
                   <td><?php echo $r['status'] ?></td>
                   <td><?php echo $r['kt_status'] ?></td>
                   <td><?php echo $r['koreksi_resep'] ?></td>
+                  <td><?php echo $r['koreksi_resep2'] ?></td>
+                  <td><?php echo $r['koreksi_resep3'] ?></td>
+                  <td><?php echo $r['koreksi_resep4'] ?></td>
+                  <td><?php echo $r['koreksi_resep5'] ?></td>
+                  <td><?php echo $r['koreksi_resep6'] ?></td>
+                  <td><?php echo $r['koreksi_resep7'] ?></td>
+                  <td><?php echo $r['koreksi_resep8'] ?></td>
                   <td><?php echo $r['percobaan_ke'] ?></td>
                   <td><?php echo $r['howmany_percobaan_ke'] ?></td>
                   <td><?php echo $r['benang_aktual'] ?></td>
@@ -191,6 +211,12 @@ include "../../koneksi.php";
                   <td><?php echo $r['final_matcher'] ?></td>
                   <td><?php echo $r['colorist1'] ?></td>
                   <td><?php echo $r['colorist2'] ?></td>
+                  <td><?php echo $r['colorist3'] ?></td>
+                  <td><?php echo $r['colorist4'] ?></td>
+                  <td><?php echo $r['colorist5'] ?></td>
+                  <td><?php echo $r['colorist6'] ?></td>
+                  <td><?php echo $r['colorist7'] ?></td>
+                  <td><?php echo $r['colorist8'] ?></td>
                   <td><?php echo $r['penanggung_jawab'] ?></td>
                   <td><?php echo $r['bleaching_tm'] ?></td>
                   <td><?php echo $r['bleaching_sh'] ?></td>
