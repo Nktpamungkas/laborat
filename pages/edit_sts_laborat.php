fbc905fb42709eb53670d0257bf9c4cbfe67cf94
ilham hdytllh <ilham.hidayatullah18@gmail.com>
2024-07-03 16:53:44 +0700

    benarkan text parisal jadi parsial

--- pages/edit_sts_laborat.php
+++ pages/edit_sts_laborat.php
@@ -4,11 +4,17 @@ session_start();
 include("../koneksi.php");
 if ($_POST) {
 	extract($_POST);
-	$id = mysqli_real_escape_string($con,$_POST['id']);
-	$sts_laborat = mysqli_real_escape_string($con,$_POST['sts_laborat']);
-	if($sts_laborat=="Waiting Approval Full"){ $stsqc="Kain OK"; $stslaborat="Approved Full";}
-	if($sts_laborat=="Waiting Approval Parsial"){ $stsqc="Kain OK Sebagian"; $stslaborat="Approved Parisal";}
-	$sqlupdate = mysqli_query($con,"UPDATE `tbl_test_qc` SET 
+	$id = mysqli_real_escape_string($con, $_POST['id']);
+	$sts_laborat = mysqli_real_escape_string($con, $_POST['sts_laborat']);
+	if ($sts_laborat == "Waiting Approval Full") {
+		$stsqc = "Kain OK";
+		$stslaborat = "Approved Full";
+	}
+	if ($sts_laborat == "Waiting Approval Parsial") {
+		$stsqc = "Kain OK Sebagian";
+		$stslaborat = "Approved Parsial";
+	}
+	$sqlupdate = mysqli_query($con, "UPDATE `tbl_test_qc` SET 
 				`sts_laborat`='$stslaborat',
 				`sts_qc`='$stsqc'
 				WHERE `id`='$id' LIMIT 1");
