6280bc9e59c28b99965d059667ff817ea9f30e89
ilham hdytllh <ilham.hidayatullah18@gmail.com>
2024-07-10 14:20:57 +0700

    membuat cetak stiker sesuai data yang di select dan membuat maksimum ada 6 stiker

--- pages/cetak/form_permintaan.php
+++ pages/cetak/form_permintaan.php
@@ -3,26 +3,16 @@ ini_set("error_reporting", 1);
 session_start();
 include "../../koneksi.php";
 
-$sql = mysqli_query($con, "SELECT * FROM tbl_test_qc WHERE sts_laborat <> 'Approved Full' ORDER BY id ASC");
-while ($r0 = mysqli_fetch_array($sql)) {
+$id_list = implode(', ', $_POST['cek']);
+$sql = mysqli_query($con, "SELECT * FROM tbl_test_qc WHERE sts_laborat <> 'Approved Full' AND id IN ($id_list) ORDER BY id ASC");
 
-	if ($_POST['cek'][$r0['id']] == $r0['id']) {
-		$idkk1 = $r0['id'] . "," . $idkk1;
-	}
+if (!$sql) {
+	echo 'Error executing query: ' . mysqli_error($con);
+	exit;
 }
-$idP = explode(",", $idkk1);
-$id_P1 = $idP[0];
-$id_P2 = $idP[1];
-$id_P3 = $idP[2];
-$data = mysqli_query($con, "SELECT * FROM tbl_test_qc WHERE id='$id_P1' ORDER BY id DESC LIMIT 1");
-$r = mysqli_fetch_array($data);
-$detail2 = explode(",", $r['permintaan_testing']);
-$data1 = mysqli_query($con, "SELECT * FROM tbl_test_qc WHERE id='$id_P2' ORDER BY id DESC LIMIT 1");
-$r1 = mysqli_fetch_array($data1);
-$detail21 = explode(",", $r1['permintaan_testing']);
-$data2 = mysqli_query($con, "SELECT * FROM tbl_test_qc WHERE id='$id_P3' ORDER BY id DESC LIMIT 1");
-$r2 = mysqli_fetch_array($data2);
-$detail22 = explode(",", $r2['permintaan_testing']);
+
+$rows = mysqli_fetch_all($sql, MYSQLI_ASSOC);
+
 ?>
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
@@ -56,163 +46,80 @@ $detail22 = explode(",", $r2['permintaan_testing']);
 <body>
 	<table width="100%" border="0" style="width: 7in;">
 		<tbody>
-			<tr>
+			<?php
+			$count = count($rows);
+			for ($i = 0; $i < $count; $i++) :
+				if ($i == 3) {
+					echo '<tr>';
+				}
+				if ($i < 6) :
+			?>
+					<td align="left" valign="top" style="height: 1.6in;">
+						<table width="100%" border="0" class="table-list1" style="width: 2.3in;">
+							<tr>
+								<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid; ">
+									<div style="font-size: 9px;"><strong>FORM PERMINTAAN TEST LABORATORY</strong></div>
+								</td>
+							</tr>
+							<tr>
+								<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
+									<div style="font-size: 8px;">BUYER : <?php echo isset($rows[$i]['buyer']) ? $rows[$i]['buyer'] : ''; ?></div>
+								</td>
+							</tr>
+							<tr>
+								<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
+									<div style="font-size: 8px;">NAMA : <?php echo isset($rows[$i]['created_by']) ? strtoupper($rows[$i]['created_by']) : ''; ?></div>
+								</td>
+							</tr>
+							<tr>
+								<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
+									<div style="font-size: 8px;">TANGGAL : <?php echo isset($rows[$i]['tgl_update']) && $rows[$i]['tgl_update'] != "" ? date('d-m-Y H:i', strtotime(substr($rows[$i]['tgl_update'], 0, 18))) : ''; ?></div>
+								</td>
+							</tr>
+							<tr>
+								<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
+									<div style="font-size: 8px;">NAMA WARNA : <?php echo isset($rows[$i]['warna']) ? $rows[$i]['warna'] : ''; ?></div>
+								</td>
+							</tr>
+							<tr>
+								<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
+									<div style="font-size: 8px;">NOMER WARNA : <?php echo isset($rows[$i]['no_warna']) ? $rows[$i]['no_warna'] : ''; ?></div>
+								</td>
+							</tr>
+							<tr>
+								<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
+									<div style="font-size: 8px;">QR CODE : <?php echo isset($rows[$i]['no_counter']) ? $rows[$i]['no_counter'] : ''; ?></div>
+								</td>
+							</tr>
+							<tr>
+								<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
+									<div style="font-size: 8px;">PERM TESTING : <?php echo isset($rows[$i]['permintaan_testing']) && ($rows[$i]['permintaan_testing'] == null || $rows[$i]['permintaan_testing'] == '') ? "FULL TEST" : $rows[$i]['permintaan_testing']; ?></div>
+								</td>
+							</tr>
+							<tr>
+								<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
+									<div style="font-size: 8px;">NOTE LAB : <?php echo isset($rows[$i]['note_laborat']) ? $rows[$i]['note_laborat'] : ''; ?></div>
+								</td>
+							</tr>
+						</table>
+					</td>
+
+			<?php
+				endif;
+			endfor;
+
+			?>
+			<?php
+			$count = count($rows);
+			if ($count == 2) :  ?>
 				<td align="left" valign="top" style="height: 1.6in;">
 					<table width="100%" border="0" class="table-list1" style="width: 2.3in;">
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid; ">
-								<div style="font-size: 9px;"><strong>FORM PERMINTAAN TEST LABORATORY</strong></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">BUYER : <?php echo $r['buyer']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">NAMA : <?php echo strtoupper($r['created_by']); ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">TANGGAL : <?php if ($r['tgl_update'] != "") {
-																			echo date('d-m-Y H:i', strtotime(substr($r['tgl_update'], 0, 18)));
-																		} ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">NAMA WARNA : <?php echo $r['warna']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">NOMER WARNA : <?php echo $r['no_warna']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">QR CODE : <?php echo $r['no_counter']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">PERM TESTING : <?php echo ($r['permintaan_testing'] == null || $r['permintaan_testing'] == '') ? "FULL TEST" : $r['permintaan_testing']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">NOTE LAB : <?php echo $r['note_laborat']; ?></div>
-							</td>
-						</tr>
 					</table>
 				</td>
-				<td align="left" valign="top" style="height: 1.6in;">
-					<table width="100%" border="0" class="table-list1" style="width: 2.3in;">
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid; ">
-								<div style="font-size: 9px;"><strong>FORM PERMINTAAN TEST LABORATORY</strong></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">BUYER : <?php echo $r['buyer']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">NAMA : <?php echo strtoupper($r['created_by']); ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">TANGGAL : <?php if ($r['tgl_update'] != "") {
-																			echo date('d-m-Y H:i', strtotime(substr($r['tgl_update'], 0, 18)));
-																		} ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">NAMA WARNA : <?php echo $r['warna']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">NOMER WARNA : <?php echo $r['no_warna']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">QR CODE : <?php echo $r['no_counter']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">PERM TESTING : <?php echo ($r['permintaan_testing'] == null || $r['permintaan_testing'] == '') ? "FULL TEST" : $r['permintaan_testing']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">NOTE LAB : <?php echo $r['note_laborat']; ?></div>
-							</td>
-						</tr>
-					</table>
-				</td>
-				<td align="left" valign="top" style="height: 1.6in;">
-					<table width="100%" border="0" class="table-list1" style="width: 2.3in;">
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid; ">
-								<div style="font-size: 9px;"><strong>FORM PERMINTAAN TEST LABORATORY</strong></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">BUYER : <?php echo $r['buyer']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">NAMA : <?php echo strtoupper($r['created_by']); ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">TANGGAL : <?php if ($r['tgl_update'] != "") {
-																			echo date('d-m-Y H:i', strtotime(substr($r['tgl_update'], 0, 18)));
-																		} ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">NAMA WARNA : <?php echo $r['warna']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">NOMER WARNA : <?php echo $r['no_warna']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">QR CODE : <?php echo $r['no_counter']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">PERM TESTING : <?php echo ($r['permintaan_testing'] == null || $r['permintaan_testing'] == '') ? "FULL TEST" : $r['permintaan_testing']; ?></div>
-							</td>
-						</tr>
-						<tr>
-							<td colspan="3" style="border-top:0px #000000 solid; border-bottom:0px #000000 solid; border-left:0px #000000 solid; border-right:0px #000000 solid;">
-								<div style="font-size: 8px;">NOTE LAB : <?php echo $r['note_laborat']; ?></div>
-							</td>
-						</tr>
-					</table>
-				</td>
-			</tr>
+			<?php endif ?>
 		</tbody>
 	</table>
 </body>
+
 
 </html>
