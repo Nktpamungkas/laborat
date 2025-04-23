5fa85f32fe75401bce1b71ac75cedd7a4229c1c1
Nktpamungkas <nktpamungkas28@gmail.com>
2025-04-23 08:37:40 +0700

    sudah ditambahin htmlspecialchars (belum di update server)

--- pages/ajax/Insert_PreliminarySchedule.php
+++ pages/ajax/Insert_PreliminarySchedule.php
@@ -3,11 +3,11 @@ ini_set("error_reporting", 1);
 include "../../koneksi.php";
 session_start();
 
-$no_resep       = $_POST['no_resep'];
-$bottle_qty_1   = $_POST['bottle_qty_1'];
-$bottle_qty_2   = $_POST['bottle_qty_2'];
-$temp_1           = $_POST['temp_1'];
-$temp_2           = $_POST['temp_2'];
+$no_resep       = htmlspecialchars($_POST['no_resep']);
+$bottle_qty_1   = htmlspecialchars($_POST['bottle_qty_1']);
+$bottle_qty_2   = htmlspecialchars($_POST['bottle_qty_2']);
+$temp_1           = htmlspecialchars($_POST['temp_1']);
+$temp_2           = htmlspecialchars($_POST['temp_2']);
 $username       = $_SESSION['userLAB'];
 
 mysqli_query($con,"INSERT INTO `tbl_preliminary_schedule` SET
