87d88381dbc427870665d488095c5ea2d56ef9a9
Nktpamungkas <nktpamungkas28@gmail.com>
2025-08-06 12:01:28 +0700

    Setiap perubahan resep harus masuk juga ke history (lain-lain > Log Perakuan Resep)

--- pages/ajax/update_suhuchamber_warna_flourescent.php
+++ pages/ajax/update_suhuchamber_warna_flourescent.php
@@ -2,6 +2,7 @@
     ini_set("error_reporting", 1);
     include "../../koneksi.php";
     session_start();
+    $time = date('Y-m-d H:i:s');
 
     $idm        = $_GET['idm'] ?? '';
     $setting    = $_POST['setting'] ?? '';
@@ -23,6 +24,17 @@
         // 2. Update tbl_matching pakai no_resep
         $update = "UPDATE tbl_matching SET $setting = '$value' WHERE no_resep = '$no_resep'";
         if (mysqli_query($con, $update)) {
+            // 3. Log perubahan
+            $SQL_rcode  = mysqli_query($con,"SELECT idm from tbl_status_matching where idm = '$idm' LIMIT 1");
+            $rcode_ = mysqli_fetch_array($SQL_rcode);
+            $ip_num = $_SERVER['REMOTE_ADDR'];
+            mysqli_query($con,"INSERT INTO log_status_matching set 
+                                    `ids` = '$rcode_[idm]',
+                                    `status` = 'selesai',
+                                    `info` = 'Perubaahan $setting menjadi $value',
+                                    `do_by` = '$_SESSION[userLAB]', 
+                                    `do_at` = '$time', 
+                                    `ip_address` = '$ip_num'");
             echo 'OK';
         } else {
             echo 'ERROR';
