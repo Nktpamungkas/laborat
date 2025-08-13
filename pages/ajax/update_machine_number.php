645dc7937ff31b419c9f9e34e48d7969f6f2ccfc
anthomars <anthomars.am@gmail.com>
2025-08-13 09:49:35 +0700

    menyempurnakan logic update mesin

--- pages/ajax/update_machine_number.php
+++ pages/ajax/update_machine_number.php
@@ -14,15 +14,21 @@ if ($id && $no_machine !== '') {
 
     if ($isOldData === 1) {
         // Cek apakah di mesin target ada data old_data
-        $check = mysqli_query($con, "
+        $checkOld = mysqli_query($con, "
             SELECT COUNT(*) AS total 
             FROM tbl_preliminary_schedule 
-            WHERE no_machine = '$no_machine' AND is_old_data = 1 AND is_old_cycle = 0 AND id_group = '$existingGroupId'
+            WHERE no_machine = '$no_machine' AND is_old_data = 1 AND is_old_cycle = 0
         ");
-        $checkRow = mysqli_fetch_assoc($check);
-        $oldCountInTarget = intval($checkRow['total']);
+        $countOld = intval(mysqli_fetch_assoc($checkOld)['total']);
 
-        if ($oldCountInTarget === 0) {
+        $checkGroup = mysqli_query($con, "
+            SELECT COUNT(*) AS total 
+            FROM tbl_preliminary_schedule 
+            WHERE no_machine = '$no_machine' AND id_group = '$existingGroupId' AND is_old_cycle = 0
+        ");
+        $countGroup = intval(mysqli_fetch_assoc($checkGroup)['total']);
+
+        if ($countOld === 0 && $countGroup > 0) {
             mysqli_query($con, "UPDATE tbl_preliminary_schedule SET is_old_data = 0 WHERE id = $id");
         }
     }
