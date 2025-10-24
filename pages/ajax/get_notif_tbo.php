35354adcf43376a24cc2747c72102ea9411017cd
tobias.mikha <tobiasmikha33@gmail.com>
2025-10-24 14:14:13 +0700

    Adjust in notification bon order

--- pages/ajax/get_notif_tbo.php
+++ pages/ajax/get_notif_tbo.php
@@ -34,95 +34,86 @@ while ($data_baru =mysqli_fetch_array($stmt_baru)){
 }
 
 // Query untuk revisi.
-$query_revisi1 = "SELECT a.*
-    FROM approval_bon_order a
-    JOIN (
-    SELECT code, MAX(id) AS max_id
-    FROM approval_bon_order
-    WHERE is_revision = 1
-    GROUP BY code
-    ) m ON m.max_id = a.id
-    WHERE a.is_revision = 1
+$query_revisi1 = "SELECT DISTINCT 
+    s.CODE
+from (
+    SELECT
+        t.CODE,
+        t.ORDERLINE,
+        case 
+            when l.revisic != REV_DEPT1 then 1
+            when l.revisic1 != REV_DEPT2 then 1
+            when l.revisic2 != REV_DEPT3 then 1
+            when l.revisic3 != REV_DEPT4 then 1
+            when l.revisic4 != REV_DEPT5 then 1
+        end as update_revisi_dept,
+        case 
+            when l.revisid != REV_COMN1 then 1
+            when l.revisi2 != REV_COMN2 then 1
+            when l.revisi3 != REV_COMN3 then 1
+            when l.revisi4 != REV_COMN4 then 1
+            when l.revisi5 != REV_COMN5 then 1
+        end as update_revisi_comn,
+        case 
+            when nullif(l.revisi1date, '') != t.REV_DATE1 then 1
+            when nullif(l.revisi2date, '') != t.REV_DATE2 then 1
+            when nullif(l.revisi3date, '') != t.REV_DATE3 then 1
+            when nullif(l.revisi4date, '') != t.REV_DATE4 then 1
+            when nullif(l.revisi5date, '') != t.REV_DATE5 then 1
+        end as update_revisi_date,
+        case 
+            when l.orderline is null then 1
+        end as baru
+    from
+        tbl_line_bonorder t
+    left join (
+        SELECT
+            distinct code,
+            status,
+            is_revision
+        from
+            approval_bon_order a
+        where
+            is_revision = 0) a on
+        a.code = t.CODE
+    left join (
+        with RankedRevisions as (
+            select
+                l.*,
+                row_number() over(
+                    partition by code, orderline
+                    order by id desc         
+                ) as rn
+            from
+                line_revision l
+        )
+        select *
+        from RankedRevisions
+        where rn = 1
+    ) l on
+        l.code = t.CODE
+        and l.orderline = t.ORDERLINE
+    where
+        coalesce(
+            t.REV_DEPT1, t.REV_DEPT2, t.REV_DEPT3, t.REV_DEPT4, t.REV_DEPT5,
+            t.REV_COMN1, t.REV_COMN2, t.REV_COMN3, t.REV_COMN4, t.REV_COMN5,
+            t.REV_DATE1, t.REV_DATE2, t.REV_DATE3, t.REV_DATE4, t.REV_DATE5
+        ) is not null
+        and a.status = 'Approved'
+        and t.IS_ACTIVE = 1
+) s
+where 
+    (
+        s.update_revisi_dept = 1
+        or s.update_revisi_comn = 1
+        or s.update_revisi_date = 1
+        or s.baru = 1 
+    );
 ";
 $stmt_revisi1 = mysqli_query($con, $query_revisi1);
-$revisi1 = [];
+$revisiListed = [];
 while ($data_revisi1 = mysqli_fetch_assoc($stmt_revisi1)) {
-    $revisi1[] = strtoupper(trim($data_revisi1['code']));
-}
-// QUERY REVISI 2 (line_revision)
-$query_revisi2 = "SELECT DISTINCT code
-    FROM line_revision
-";
-$stmt_revisi2 = mysqli_query($con, $query_revisi2);
-$revisi2 = [];
-while ($data_revisi2 = mysqli_fetch_assoc($stmt_revisi2)) {
-    $revisi2[] = strtoupper(trim($data_revisi2['code']));
-}
-
-
-$gabRevisi = array_values(array_unique(array_merge($revisi1, $revisi2)));
-
-// QUERY HEADER DB2
-$query_head = "SELECT DISTINCT CODE
-    FROM tbl_header_bonorder t
-    WHERE COALESCE(
-        t.REV_DEPT1, t.REV_DEPT2, t.REV_DEPT3, t.REV_DEPT4, t.REV_DEPT5,
-        t.REV_COMN1, t.REV_COMN2, t.REV_COMN3, t.REV_COMN4, t.REV_COMN5,
-        t.REV_DATE1, t.REV_DATE2, t.REV_DATE3, t.REV_DATE4, t.REV_DATE5
-    ) IS NOT NULL
-    AND t.BUYER IS NOT NULL
-    AND t.IS_ACTIIVE = 1
-    AND t.APPROVED_RMP_DATETIME IS NOT NULL
-";
-$stmt_revisi_head = mysqli_query($con, $query_head);
-$head = [];
-while ($data_head = mysqli_fetch_assoc($stmt_revisi_head)) {
-    $head[] = strtoupper(trim($data_head['CODE']));
-}
-
-// QUERY LINE DB2
-$query_line = "SELECT DISTINCT t.CODE
-    FROM tbl_line_bonorder t
-    LEFT JOIN tbl_header_bonorder h ON h.CODE = t.CODE
-    WHERE COALESCE(
-        t.REV_DEPT1, t.REV_DEPT2, t.REV_DEPT3, t.REV_DEPT4, t.REV_DEPT5,
-        t.REV_COMN1, t.REV_COMN2, t.REV_COMN3, t.REV_COMN4, t.REV_COMN5,
-        t.REV_DATE1, t.REV_DATE2, t.REV_DATE3, t.REV_DATE4, t.REV_DATE5
-    ) IS NOT NULL
-    AND t.BUYER IS NOT NULL
-    AND t.IS_ACTIVE = 1
-    AND h.APPROVED_RMP_DATETIME IS NOT NULL
-";
-$stmt_revisi_line = mysqli_query($con, $query_line);
-$line = [];
-while ($data_line = mysqli_fetch_assoc($stmt_revisi_line)) {
-    $line[] = strtoupper(trim($data_line['CODE']));
-}
-
-
-$gabRevisiD = array_values(array_unique(array_merge($head, $line)));
-
-$gabRevisi = array_filter($gabRevisi);
-$gabRevisiD = array_filter($gabRevisiD);
-$arrayBaru = array_values(array_diff($gabRevisiD, $gabRevisi));
-
-// Query yang data rejected 
-$query_approved = "SELECT DISTINCT 
-                            code
-                        from
-                            approval_bon_order
-                        where
-                            is_revision = 0
-                        and status = 'Rejected'
-";
-$stmt_rejected = mysqli_query($con, $query_approved);
-$rejected = [];
-while ($data_reject = mysqli_fetch_assoc($stmt_rejected)) {
-    $rejected[] = strtoupper(trim($data_reject['code']));
-}
-
-$revisiListed = array_values(array_diff($arrayBaru, $rejected));
-foreach($revisiListed as $data_revisi){
+    $revisiListed[] = strtoupper(trim($data_revisi1['CODE']));
     $countRev += 1;
 }
 
