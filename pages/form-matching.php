53441d6683742decf3c23a930edef3d33653a356
Anthomars <joko.susanto@indotaichen.com>
2025-11-25 16:00:57 +0700

    mencegah duplikat saat bikin kartu matching

--- pages/form-matching.php
+++ pages/form-matching.php
@@ -2602,5 +2602,12 @@
 		}
 	});
 </script>
+<script>
+	$(function() {
+		$('form.form-horizontal').on('submit', function(e) {
+			$(this).find('button[name="simpan"]').prop('disabled', true);
+		});
+	});
+</script>
 
 </html>
