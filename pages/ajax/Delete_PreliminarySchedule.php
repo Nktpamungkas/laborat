0f407c8044ef03f41e48aaeca82c47e91c126dda
riyanuari-indotaichen <riyanuari.rachmat@indotaichen.com>
2026-01-05 14:46:23 +0700

    fix: revert delete manual tbl_preliminary_schedule_element

--- pages/ajax/Delete_PreliminarySchedule.php
+++ pages/ajax/Delete_PreliminarySchedule.php
@@ -6,12 +6,10 @@ if (isset($_POST['id'])) {
     $id = (int) $_POST['id'];
     $query = "DELETE FROM tbl_preliminary_schedule WHERE id = $id";
     if (mysqli_query($con, $query)) {
-        $query2 = "DELETE FROM tbl_preliminary_schedule_element WHERE tbl_preliminary_schedule_id = $id";
-        if (mysqli_query($con, $query2)) {
-            echo json_encode(['status' => 'success']);
-        } else {
-            echo json_encode(['status' => 'error', 'message' => mysqli_error($con)]);
-        }
+        // $query2 = "DELETE FROM tbl_preliminary_schedule_element WHERE tbl_preliminary_schedule_id = $id";
+        // mysqli_query($con, $query2);
+
+        echo json_encode(['status' => 'success']);
     } else {
         echo json_encode(['status' => 'error', 'message' => mysqli_error($con)]);
     }
