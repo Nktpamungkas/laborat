bcbd0145d952e15fd3d2381d19e75a81ea447025
Asep Rifki <rifkipauji993@gmail.com>
2026-02-03 09:09:42 +0700

    fix filter summary by category pemakaian gd. kimia if transfer from anotherwarehouser except M101 and M501

--- pages/pemakaian_obat_category.php
+++ pages/pemakaian_obat_category.php
@@ -424,10 +424,13 @@ $Bulan = $Thn2 . "-" . $Bln2;
                                         -- AND s.TRANSACTIONDATE BETWEEN '$_POST[tgl]' AND '$_POST[tgl2]'
                                         AND TIMESTAMP(s.TRANSACTIONDATE, s.TRANSACTIONTIME) BETWEEN '$awal 23:01:00' AND '$_POST[tgl2] 23:00:00'
                                         AND s.TEMPLATECODE IN ('QCT','304','OPN','204','125')
-                                        AND NOT COALESCE(TRIM( CASE 
+                                       AND NOT COALESCE(TRIM( CASE 
                                                                 WHEN s3.TEMPLATECODE IS NOT NULL THEN s3.TEMPLATECODE
                                                                 ELSE s.TEMPLATECODE
-                                                            END), '') || COALESCE(TRIM(s.LOGICALWAREHOUSECODE), '')  IN ('OPNM101','303M101','304M510')
+                                                            END), '') || COALESCE(TRIM(CASE 
+                                                                WHEN  s3.LOGICALWAREHOUSECODE IS NOT NULL THEN  s3.LOGICALWAREHOUSECODE
+                                                                ELSE  s.LOGICALWAREHOUSECODE
+                                                            END), '')  IN ('OPNM101','303M101','304M510')
                                         and s.CREATIONUSER != 'MT_STI'
                                         AND s.LOGICALWAREHOUSECODE $where_warehouse 
                                         and s.DECOSUBCODE01 = '$row[DECOSUBCODE01]'
